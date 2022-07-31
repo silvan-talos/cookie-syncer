b89047fd812f2153bbb48fa2cac51e322cd8bb47
Silvan Talos <silvantalos@ymail.com>
2022-08-01 02:25:02 +0300

    Add database initial partners for tests

--- MySQL/test.sql
+++ MySQL/test.sql
@@ -18,3 +18,8 @@ CREATE TABLE IF NOT EXISTS `Sync` (
   `OtherPartnerID` varchar(129) DEFAULT NULL,
   `OtherPartnerUserID` varchar(129) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- add inital partners for tests
+INSERT INTO Partner(ID, Name, URL)
+Values("3894994b-7fef-402b-8344-1d3359c5de93","DSP","http://127.0.0.1:8080"),
+("c408fefb-38a3-44cc-8f40-82c0af0b35a4","SSP","http://127.0.0.1:8080");
